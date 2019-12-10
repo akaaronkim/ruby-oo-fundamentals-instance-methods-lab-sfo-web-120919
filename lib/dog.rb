206b6e914ae396685d34996cfc177c9afed00766
Aaron Kim <akaaronkim@gmail.com>
2019-12-10 21:25:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.bark
+
+class Person
+  def talk
+    puts "Hello World"
+  end
+end
